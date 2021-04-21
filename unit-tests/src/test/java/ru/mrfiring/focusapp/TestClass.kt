0e8a109a1d34715f6a4167d282ca12edf67bba2e
Kirill Bondarev <Gut54566@yandex.ru>
2021-04-21 22:26:04 +0700

    Initial commit

---
+++ unit-tests/src/test/java/ru/mrfiring/focusapp/TestClass.kt
@@ -0,0 +1,14 @@
+package ru.mrfiring.focusapp
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+
+class TestClass {
+
+    @Test
+    fun hello(){
+        assertEquals("hello", "hello")
+    }
+
+}
