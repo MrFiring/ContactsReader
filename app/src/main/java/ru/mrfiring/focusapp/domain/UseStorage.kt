50e7fac94659634cabc574510ed14cb4c5fde7d4
Kirill Bondarev <Gut54566@yandex.ru>
2021-05-11 00:39:29 +0700

    UseStorage refactored to provide more than 2 custom storages and improved code readability.

--- app/src/main/java/ru/mrfiring/focusapp/domain/UseStorage.kt
+++ app/src/main/java/ru/mrfiring/focusapp/domain/UseStorage.kt
@@ -2,5 +2,19 @@ package ru.mrfiring.focusapp.domain
 
 enum class UseStorage(val value: Int) {
     FILE(0),
-    DATABASE(1)
+    DATABASE(1);
+
+    fun next(): UseStorage = when (value) {
+        0 -> DATABASE
+        1 -> FILE
+        else -> DATABASE
+    }
+
+    companion object {
+        fun parse(type: Int): UseStorage = when (type) {
+            0 -> FILE
+            1 -> DATABASE
+            else -> DATABASE
+        }
+    }
 }
--- app/src/main/java/ru/mrfiring/focusapp/presentation/detail/DetailViewModel.kt
+++ app/src/main/java/ru/mrfiring/focusapp/presentation/detail/DetailViewModel.kt
@@ -6,7 +6,7 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import ru.mrfiring.focusapp.domain.UseStorage
 import ru.mrfiring.focusapp.domain.model.DomainContact
 import ru.mrfiring.focusapp.domain.usecase.GetContactWithIdFromStorageUseCase
-import ru.mrfiring.focusapp.domain.usecase.GetIsUseDBStorageByDefaultUseCase
+import ru.mrfiring.focusapp.domain.usecase.GetUseStorageUseCase
 import ru.mrfiring.focusapp.domain.usecase.UpdateContactUseCase
 import ru.mrfiring.focusapp.presentation.BaseViewModel
 import javax.inject.Inject
@@ -14,7 +14,7 @@ import javax.inject.Inject
 class DetailViewModel @Inject constructor(
     private val getContactWithIdFromStorageUseCase: GetContactWithIdFromStorageUseCase,
     private val updateContactUseCase: UpdateContactUseCase,
-    private val getIsUseDBStorageByDefaultUseCase: GetIsUseDBStorageByDefaultUseCase
+    private val getUseStorageUseCase: GetUseStorageUseCase
 ) : BaseViewModel() {
 
     private val _detailState = MutableLiveData<DetailState>()
@@ -31,10 +31,10 @@ class DetailViewModel @Inject constructor(
             DetailState.Loading
         )
 
-        getIsUseDBStorageByDefaultUseCase()
+        getUseStorageUseCase()
             .observeOn(AndroidSchedulers.mainThread())
-            .subscribe { isDBStorage ->
-                _useStorage.value = if (isDBStorage) UseStorage.DATABASE else UseStorage.FILE
+            .subscribe { storage ->
+                _useStorage.value = storage
             }
             .untilDestroy()
     }
